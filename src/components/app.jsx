01f3d5ae60d74bf249bd5f88bc65f5c68a0f8df8
Jeong Min Lee <jacklee0223@gmail.com>
2017-05-06 23:35:34 -0700

    render app component to root

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../style/main.css';
+
+export default class app extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
