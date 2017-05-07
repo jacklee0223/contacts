315173be90892e2947421b35b3601bbaca78ad19
Jeong Min Lee <jacklee0223@gmail.com>
2017-05-06 23:38:23 -0700

    add navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import '../style/main.css';
 
+import Navbar from './navbar';
+
 export default class app extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
+
       </div>
     );
   }
