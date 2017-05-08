d440899b30c8191382f4e42c8ec8bc0f00daca45
Jeong Min Lee <jacklee0223@gmail.com>
2017-05-08 12:23:30 -0700

    clean up add contact modal footer

--- src/containers/add_contact.jsx
+++ src/containers/add_contact.jsx
@@ -80,64 +80,69 @@ class AddContact extends Component {
   }
 
   renderForm() {
+    let close = () => this.setState({show: false});
+    
     return (
       <Form inline>
-        <FormGroup className="add-contact-form" controlId="formInlineFirstName">
-          <ControlLabel>First Name</ControlLabel>
-          {' '}
-          <FormControl type="text" placeholder="First Name" name="first_name" inputRef={ref => {
-            this.input = ref
-          }} value={this.state.first_name} onChange={this.handleChange}/>
-        </FormGroup>
-        {' '}
-        <FormGroup className="add-contact-form" controlId="formInlineLastName">
-          <ControlLabel>Last Name</ControlLabel>
-          {' '}
-          <FormControl type="text" placeholder="Last Name" name="last_name" inputRef={ref => {
-            this.input = ref
-          }} value={this.state.last_name} onChange={this.handleChange}/>
-        </FormGroup>
-        {' '}
-        <FormGroup className="add-contact-form" controlId="formInlinePhone">
-          <ControlLabel>Phone</ControlLabel>
-          {' '}
-          <FormControl type="tel" placeholder="Phone" name="phone" value={this.state.phone} onChange={this.handleChange}/>
-        </FormGroup>
-        {' '}
-        <FormGroup className="add-contact-form" controlId="formInlineEmail">
-          <ControlLabel>Email</ControlLabel>
-          {' '}
-          <FormControl type="email" placeholder="Email" name="email" inputRef={ref => {
-            this.input = ref
-          }} value={this.state.email} onChange={this.handleChange}/>
-        </FormGroup>
-        {' '}
-        <FormGroup className="add-contact-form" controlId="formInlineDOB">
-          <ControlLabel>Date of Birth</ControlLabel>
-          {' '}
-          <FormControl type="date" placeholder="Date of Birth" name="dob" inputRef={ref => {
-            this.input = ref
-          }} value={this.state.dob} onChange={this.handleChange}/>
-        </FormGroup>
-        {' '}
-        <FormGroup className="add-contact-form" controlId="formInlineNote">
-          <ControlLabel>Note</ControlLabel>
-          {' '}
-          <FormControl type="text" placeholder="Notes" name="notes" inputRef={ref => {
-            this.input = ref
-          }} value={this.state.notes} onChange={this.handleChange}/>
-        </FormGroup>
-        {' '}
-        <Button type="submit" onClick={this.onAddContact}>
-          Add Contact
-        </Button>
-      </Form>
+        <Modal.Body>
+            <FormGroup className="add-contact-form" controlId="formInlineFirstName">
+              <ControlLabel>First Name</ControlLabel>
+              {' '}
+              <FormControl type="text" placeholder="First Name" name="first_name" inputRef={ref => {
+                this.input = ref
+              }} value={this.state.first_name} onChange={this.handleChange}/>
+            </FormGroup>
+            {' '}
+            <FormGroup className="add-contact-form" controlId="formInlineLastName">
+              <ControlLabel>Last Name</ControlLabel>
+              {' '}
+              <FormControl type="text" placeholder="Last Name" name="last_name" inputRef={ref => {
+                this.input = ref
+              }} value={this.state.last_name} onChange={this.handleChange}/>
+            </FormGroup>
+            {' '}
+            <FormGroup className="add-contact-form" controlId="formInlinePhone">
+              <ControlLabel>Phone</ControlLabel>
+              {' '}
+              <FormControl type="tel" placeholder="Phone" name="phone" value={this.state.phone} onChange={this.handleChange}/>
+            </FormGroup>
+            {' '}
+            <FormGroup className="add-contact-form" controlId="formInlineEmail">
+              <ControlLabel>Email</ControlLabel>
+              {' '}
+              <FormControl type="email" placeholder="Email" name="email" inputRef={ref => {
+                this.input = ref
+              }} value={this.state.email} onChange={this.handleChange}/>
+            </FormGroup>
+            {' '}
+            <FormGroup className="add-contact-form" controlId="formInlineDOB">
+              <ControlLabel>Date of Birth</ControlLabel>
+              {' '}
+              <FormControl type="date" placeholder="Date of Birth" name="dob" inputRef={ref => {
+                this.input = ref
+              }} value={this.state.dob} onChange={this.handleChange}/>
+            </FormGroup>
+            {' '}
+            <FormGroup className="add-contact-form" controlId="formInlineNote">
+              <ControlLabel>Note</ControlLabel>
+              {' '}
+              <FormControl type="text" placeholder="Notes" name="notes" inputRef={ref => {
+                this.input = ref
+              }} value={this.state.notes} onChange={this.handleChange}/>
+            </FormGroup>
+            {' '}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit" onClick={this.onAddContact}>
+            Add Contact
+          </Button>
+          <Button onClick={close}>Close</Button>
+        </Modal.Footer>
+    </Form>
     )
   }
 
   render() {
-    let close = () => this.setState({show: false});
-
     return (
       <div className="add-contact-container">
         <div className="modal-container">
@@ -149,12 +154,7 @@ class AddContact extends Component {
             <Modal.Header closeButton>
               <Modal.Title id="contained-modal-title">Add Contact</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-              {this.renderForm()}
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={close}>Close</Button>
-            </Modal.Footer>
+            {this.renderForm()}
           </Modal>
         </div>
       </div>
